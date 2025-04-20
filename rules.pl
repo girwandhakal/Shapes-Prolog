b642357da551408f79bf9c15ae623046daa3bd5e
girwandhakal <girwandhakal@gmail.com>
2025-04-20 14:04:47 -0500

    fixed rules.pl

--- rules.pl
+++ rules.pl
@@ -1,6 +1,13 @@
 pay(F, L, P) :- salaried(F,L,Salary), P is Salary.
 
-pay(F, L, P) :- hourly(F,L,Hours,Rate), P is Hours*Rate.
+pay(F, L, P) :- hourly(F,L,Hours,Rate), 
+                Hours =< 40 -> P is Hours * Rate.
+
+pay(F, L, P) :- hourly(F,L,Hours,Rate), 
+                Hours =< 50 -> P is (40*Rate) + ((Hours-40)*Rate*1.5).
+
+pay(F, L, P) :- hourly(F,L,Hours,Rate), 
+                Hours > 50 -> P is ((40*Rate) + (10*1.5*Rate) + ((Hours-50)*Rate*2)).
 
 pay(F, L, P) :- commission(F, L, Min, Sales, Rate), 
                 (Sales*Rate < Min -> P = Min ; P is Sales * Rate).
