64739f88a33adf675c0c951ed824729e611df5c1
girwandhakal <girwandhakal@gmail.com>
2025-04-20 15:47:05 -0500

    added functions.pl

---
+++ functions.pl
@@ -0,0 +1,66 @@
+list(Op, Ref, List) :-
+    (
+        (Op = ('=='), findall([X,Y,Z], (pay(X, Y, Z), Z =:= Ref), List));
+        (Op = ('!='), findall([X,Y,Z], (pay(X, Y, Z), Z =\= Ref), List));
+        (Op = ('>'),  findall([X,Y,Z], (pay(X, Y, Z), Z >  Ref), List));
+        (Op = ('>='), findall([X,Y,Z], (pay(X, Y, Z), Z >= Ref), List));
+        (Op = ('<'),  findall([X,Y,Z], (pay(X, Y, Z), Z <  Ref), List));
+        (Op = ('<='), findall([X,Y,Z], (pay(X, Y, Z), Z =< Ref), List))
+    ).
+
+count(Op, Ref, Count) :-
+    (
+        (Op = ('=='), findall(Z, (pay(_, _, Z), Z =:= Ref), List), length(List, Count));
+        (Op = ('!='), findall(Z, (pay(_, _, Z), Z =\= Ref), List), length(List, Count));
+        (Op = ('>'),  findall(Z, (pay(_, _, Z), Z >  Ref), List), length(List, Count));
+        (Op = ('>='), findall(Z, (pay(_, _, Z), Z >= Ref), List), length(List, Count));
+        (Op = ('<'),  findall(Z, (pay(_, _, Z), Z <  Ref), List), length(List, Count));
+        (Op = ('<='), findall(Z, (pay(_, _, Z), Z =< Ref), List), length(List, Count))
+    ).
+
+min(Op, Ref, Min) :-
+    (
+        (Op = ('=='), findall(Z, (pay(_, _, Z), Z =:= Ref), List), min_list(List, Min));
+        (Op = ('!='), findall(Z, (pay(_, _, Z), Z =\= Ref), List), min_list(List, Min));
+        (Op = ('>'),  findall(Z, (pay(_, _, Z), Z >  Ref), List), min_list(List, Min));
+        (Op = ('>='), findall(Z, (pay(_, _, Z), Z >= Ref), List), min_list(List, Min));
+        (Op = ('<'),  findall(Z, (pay(_, _, Z), Z <  Ref), List), min_list(List, Min));
+        (Op = ('<='), findall(Z, (pay(_, _, Z), Z =< Ref), List))
+    ).
+
+max(Op, Ref, Max) :-
+    (
+        (Op = ('=='), findall(Z, (pay(_, _, Z), Z =:= Ref), List), max_list(List, Max));
+        (Op = ('!='), findall(Z, (pay(_, _, Z), Z =\= Ref), List), max_list(List, Max));
+        (Op = ('>'),  findall(Z, (pay(_, _, Z), Z >  Ref), List), max_list(List, Max));
+        (Op = ('>='), findall(Z, (pay(_, _, Z), Z >= Ref), List), max_list(List, Max));
+        (Op = ('<'),  findall(Z, (pay(_, _, Z), Z <  Ref), List), max_list(List, Max));
+        (Op = ('<='), findall(Z, (pay(_, _, Z), Z =< Ref), List))
+    ).
+
+total(Op, Ref, Total) :-
+    (
+        (Op = ('=='), findall(Z, (pay(_, _, Z), Z =:= Ref), List), sum_list(List, Total));
+        (Op = ('!='), findall(Z, (pay(_, _, Z), Z =\= Ref), List), sum_list(List, Total));
+        (Op = ('>'),  findall(Z, (pay(_, _, Z), Z >  Ref), List), sum_list(List, Total));
+        (Op = ('>='), findall(Z, (pay(_, _, Z), Z >= Ref), List), sum_list(List, Total));
+        (Op = ('<'),  findall(Z, (pay(_, _, Z), Z <  Ref), List), sum_list(List, Total));
+        (Op = ('<='), findall(Z, (pay(_, _, Z), Z =< Ref), List))
+    ).
+
+average_list([], 0).
+average_list(List, Avg) :-
+    sum_list(List, Sum),
+    length(List, Len),
+    Len > 0,
+    Avg is Sum / Len.
+
+avg(Op, Ref, Avg) :-
+    (
+        (Op = ('=='), findall(Z, (pay(_, _, Z), Z =:= Ref), List), average_list(List, Avg));
+        (Op = ('!='), findall(Z, (pay(_, _, Z), Z =\= Ref), List), average_list(List, Avg));
+        (Op = ('>'),  findall(Z, (pay(_, _, Z), Z >  Ref), List), average_list(List, Avg));
+        (Op = ('>='), findall(Z, (pay(_, _, Z), Z >= Ref), List), average_list(List, Avg));
+        (Op = ('<'),  findall(Z, (pay(_, _, Z), Z <  Ref), List), average_list(List, Avg));
+        ((Op = '<='), findall(Z, (pay(_, _, Z), Z =< Ref), List), average_list(List, Avg))
+    ).
